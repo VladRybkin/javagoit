d7cb6602e3757b5ea845f68992314f1a420ee46d
VladRybkin <Footballer0@mail.ru>
2016-09-20 21:00:28 +0300

    11

--- src/src/module5/TripAdvisorAPI.java
+++ src/src/module5/TripAdvisorAPI.java
@@ -11,13 +11,13 @@ public class TripAdvisorAPI implements API {
     public TripAdvisorAPI() {
         Room room1 = new Room(1, 200, 3, new Date(20072016), "Kyivhotel", "Kyiv");
         rooms[0]=room1;
-        Room room2 = new Room(2, 200, 3, new Date(20072016), "Kyivhotel", "Kyiv");
+        Room room2 = new Room(2, 250, 3, new Date(20072016), "Kyivhotel", "Kyiv");
         rooms[01]=room2;
-        Room room3 = new Room(3, 200, 3, new Date(20072016), "Kyivhotel", "Kyiv");
+        Room room3 = new Room(3, 200, 6, new Date(20072016), "Kyivhotel", "Kyiv");
         rooms[2]=room3;
-        Room room4 = new Room(4, 200, 3, new Date(20072016), "Kyivhotel", "Kyiv");
+        Room room4 = new Room(4, 200, 4, new Date(20072016), "Kyivhotel", "Kyiv");
         rooms[3]=room4;
-        Room room5 = new Room(5, 200, 3, new Date(20072016), "Kyivhotel", "Kyiv");
+        Room room5 = new Room(5, 400, 7, new Date(20072016), "Kyivhotel", "Kyiv");
         rooms[4]=room5;
         rooms =new Room[]{room1, room2, room3, room4, room5};
 
