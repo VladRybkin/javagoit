5a281dec8bca27a947bd7d13197f74f35a2ca070
VladRybkin <Footballer0@mail.ru>
2016-09-21 22:23:42 +0300

    11

--- src/src/module5/GoogleAPI.java
+++ src/src/module5/GoogleAPI.java
@@ -9,17 +9,12 @@ import java.util.Date;
 public class GoogleAPI implements API {
     private Room[] rooms  = new Room[5];
     public GoogleAPI() {
-        Room room1 = new Room(1, 200, 3, new Date(20072016), "Kyivhotel", "Kyiv");
-        rooms[0]=room1;
-        Room room2 = new Room(2, 250, 3, new Date(20072016), "Kyivhotel", "Kyiv");
-        rooms[01]=room2;
-        Room room3 = new Room(3, 200, 6, new Date(20072016), "Kyivhotel", "Kyiv");
-        rooms[2]=room3;
-        Room room4 = new Room(4, 200, 4, new Date(20072016), "Kyivhotel", "Kyiv");
-        rooms[3]=room4;
-        Room room5 = new Room(5, 400, 7, new Date(20072016), "Kyivhotel", "Kyiv");
-        rooms[4]=room5;
-        rooms =new Room[]{room1, room2, room3, room4, room5};
+        rooms[0] = new Room(1, 200, 3, new Date(20072016), "Kyivhotel", "Kyiv");
+        rooms[1] = new Room(2, 250, 3, new Date(20072016), "Kyivhotel", "Kyiv");
+        rooms[2] = new Room(3, 200, 6, new Date(20072016), "Kyivhotel", "Kyiv");
+        rooms[3] = new Room(4, 200, 4, new Date(20072016), "Kyivhotel", "Kyiv");
+        rooms[4] = new Room(5, 400, 7, new Date(20072016), "Kyivhotel", "Kyiv");
+        rooms =new Room[]{rooms[0], rooms[1],rooms[2],rooms[3],rooms[4],};
         //long id, int price, int persons, Date dateAvailableFrom, String hotelName, String cityName
     }
 
