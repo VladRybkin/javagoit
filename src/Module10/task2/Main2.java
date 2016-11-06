d87af2e9168c11520542bf5f4b8351810f1b2f61
VladRybkin <Footballer0@mail.ru>
2016-11-06 18:50:36 +0200

    11

--- src/Module10/task2/Main2.java
+++ src/Module10/task2/Main2.java
@@ -4,16 +4,16 @@ package Module10.task2;
  * Created by Vlad on 02.11.2016.
  */
 public class Main2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException{
     try{
-        amount(-1);
-    } catch (Exception e){
-        System.out.println(e);
-        throw e;
+       throw new MyException("Exception");
+    } catch (MyException e){
+        System.err.println(e.getMessage());
+    }finally {
+        System.out.println("finally");
     }
     }
-        static void amount(int a) {
-        if (a < 0) throw new MyException("a<0");
-        System.out.println("Result is : " + a);
-    }
+
+
+
 }
--- src/Module10/task2/MyException.java
+++ src/Module10/task2/MyException.java
@@ -3,7 +3,7 @@ package Module10.task2;
 /**
  * Created by Vlad on 02.11.2016.
  */
-public class MyException extends RuntimeException {
+public class MyException extends Exception {
     private String e;
 
     public MyException(String e) {
